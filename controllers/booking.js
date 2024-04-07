928799176503df729ba0e733e1ea5269e0389348
tumrabert <tum_tanakid@hotmail.com>
2024-04-07 19:14:19 +0700

    booking models and controller.

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,4 +1,4 @@
-const Appointment = require("../models/Appointment");
+const Booking = require("../models/Booking");
 
 // @desc Get all appointments
 // @route GET /api/v1/appointments
@@ -10,24 +10,24 @@ exports.getAppointments = async (req, res, next) => {
     query = Appointment.find({ user: req.user.id });
   } else {
     //If you are an admin, you can see all!
-    if (req.params.hospitalId) {
-      console.log(req.params.hospitalId);
-      query = Appointment.find({ hospital: req.params.hospitalId });
-    } else query = Appointment.find();
+    if (req.params.campgroundId) {
+      console.log(req.params.campgroundId);
+      query = Booking.find({ campground: req.params.campgroundId });
+    } else query = Booking.find();
   }
 
   try {
-    const appointments = await query;
+    const booking = await query;
 
     res.status(200).json({
       success: true,
-      count: appointments.length,
-      data: appointments,
+      count: booking.length,
+      data: booking,
     });
   } catch (error) {
     console.log(error);
     res
       .status(500)
-      .json({ success: false, message: "Cannot find Appointment" });
+      .json({ success: false, message: "Cannot find Booking" });
   }
 };
